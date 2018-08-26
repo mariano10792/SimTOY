0395d9eaeae5ec1e3bc4753be63b407333deeeb9
mariano10792 <mariano10792@gmail.com>
2018-08-26 19:36:35 -0300

    merging funciona. Al archivo 128.fits lo llame 30.fits y mate al 30 anterior porque estaba vacio y cagaba le codigo. CUIDADO: este repositorio (casa) y el de el laburo tienen distitnos archivos ya que el del laburo tiene todo lo de la corrida nueva (creo)

--- mejor/merging.py
+++ mejor/merging.py
@@ -1,35 +1,57 @@
 from astropy.io import fits
 
 
-arrayhdu = ()
-count= 73
-while (count < 267)
-
-	
-	array = (array, count)
-	hdu = fits.open('/home/mariano/Desktop/caca/proc_fe552018-04-14_0142.fits')
-	arrayhdu[count]=hdu
-	
-	
-	
-	
-	
-	
-	
-	
+#arrayhdu = ()
 
 
-hdulist2 = fits.open('/home/mariano/Desktop/caca/proc_fe552018-04-14_0143.fits')
+hduinit = fits.open('/home/mariano/Desktop/SimTOY/mejor/1.fits')
+tinit=hduinit[3].data
+hduinit = fits.PrimaryHDU(tinit)
+hduinitl = fits.HDUList([hduinit])
+hduinitl.writeto('merge1.fits')
 
 
-t1=hdulist[3].data
-t2=hdulist2[3].data
 
-T=t1+t2
+count=2
+while (count < 127):
+        coun=count-1
+        Coun=str(coun)
+        Count=str(count)
+        hdu0=fits.open('/home/mariano/Desktop/SimTOY/mejor/merge'+Coun+'.fits')
+        t0=hdu0[0].data
 
-hdu = fits.PrimaryHDU(T)
-hdul = fits.HDUList([hdu])
-hdul.writeto('merge142143.fits')
+	hdu = fits.open('/home/mariano/Desktop/SimTOY/mejor/'+Count+'.fits')
+        t=hdu[3].data
+
+        T=t0+t
+
+#        arrayhdu[count]=hdu
+	
+
+        hdusave = fits.PrimaryHDU(T)
+        hdusavel = fits.HDUList([hdusave])
+        hdusavel.writeto('merge'+Count+'.fits')
+	
+	
+	count+=1
+	
+	
+print("termine")
+	
+
+
+#hdulist2 = fits.open('/home/mariano/Desktop/caca/proc_fe552018-04-14_0143.fits')
+
+
+#t1=hdulist[3].data
+#t2=hdulist2[3].data
+
+#T=t1+t2
+
+#hdu = fits.PrimaryHDU(T)
+#hdul = fits.HDUList([hdu])
+#hdul.writeto('merge142143.fits')
+
 
 
 
